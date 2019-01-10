67411b250f4ce8892b07bf9a28aa20d49964bf9a
Diana Zhilinskaya <zilinskaadiana7@gmail.com>
2019-01-11 00:40:09 +0300

    Framework222

---
+++ Framework222/Framework222/Instance/Driver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Security.Policy;
+
+namespace _10UnitTests
+{
+    class Driver
+    {
+        private static IWebDriver driver;
+        private static WebDriverWait wait;
+
+        private Driver() { }
+
+        public static IWebDriver GetInstance()
+        {
+            if (driver == null)
+            {
+                //driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+                driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                driver.Manage().Window.Size = new System.Drawing.Size(driver.Manage().Window.Size.Width / 2, driver.Manage().Window.Size.Height);
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(90));
+                // driver.Manage().Timeouts().ImplicitWait.Add(TimeSpan.FromSeconds(30));
+                //driver.Manage().Window.Maximize();
+            }
+            return driver;
+        }
+        public static void CloseBrowser()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+    }
+}
---
+++ Framework222/Framework222/Steps/Steps.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Security.Policy;
+using System.Threading;
+
+namespace _10UnitTests
+{
+    class Steps
+    {
+        IWebDriver driver;
+        public void InitBrowser()
+        {
+            driver = _10UnitTests.Driver.GetInstance();
+        }
+
+        public void CloseBrowser()
+        {
+            _10UnitTests.Driver.CloseBrowser();
+        }
+
+        public bool SelectFirstTrip(string departureName, string destinationName)
+        {
+            _10UnitTests.MainPage mainPage = new _10UnitTests.MainPage(driver);
+            mainPage.InsertFirstTrip(departureName, destinationName);
+          
+            return true;
+        }
+
+        public bool SelectFirstTripDate()
+        {
+            _10UnitTests.MainPage mainPage = new _10UnitTests.MainPage(driver);
+            mainPage.ChooseFirstTripDate();
+            return true;
+        }
+
+        public bool SelectSecondTrip(string departureName, string destinationName)
+        {
+            _10UnitTests.MainPage mainPage = new _10UnitTests.MainPage(driver);
+            mainPage.InsertSecondTrip(destinationName, departureName);
+          
+            return true;
+        }
+
+        public bool SelectSecondTripDate()
+        {
+            _10UnitTests.MainPage mainPage = new _10UnitTests.MainPage(driver);
+            mainPage.ChooseSecondTripDate();
+            return true;
+        }
+
+        public bool StartSearchTickets()
+        {
+            _10UnitTests.MainPage mainPage = new _10UnitTests.MainPage(driver);
+            try
+            {
+                mainPage.ClickOnSearchTickets();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void SelectPage()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            selectPage.OpenPage();        
+        }
+
+        public void SelectComplexFlight()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            selectPage.SelectComplexFlight();
+        }
+
+        public void SelectOneSideFlight()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            selectPage.SelectOneSideFlight();
+        }
+
+        public void SelectBothFlightTrip()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            selectPage.SelectBothSides();
+
+        }
+
+        public bool SelectDatesForBothSides()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+             return selectPage.SelectDatesForBothSides(); ;
+        }
+
+        public bool SelectPassengers()
+        {
+            _10UnitTests.MainPage mainPage = new _10UnitTests.MainPage(driver);
+            mainPage.ClickPassengersMenu();
+            mainPage.SelectPassengerAdult();
+            for(int i=0;i<2;i++)
+            mainPage.SelectPassengerBaby();
+            if (Int32.Parse(mainPage.GetCountBaby()) <= Int32.Parse(mainPage.GetCountAdult()))
+            {
+                return true;
+            }
+            else
+            return false;
+        }
+        public bool SelectPassengersWithoutAdults()
+        {
+            _10UnitTests.MainPage mainPage = new _10UnitTests.MainPage(driver);
+            mainPage.ClickPassengersMenu();
+            mainPage.SelectPassengerBaby();
+            if (Int32.Parse(mainPage.GetCountBaby()) > 0 && Int32.Parse(mainPage.GetCountAdult()) == 0)
+                return true;
+            else return false;
+        }
+
+        public bool SelectDepartureDateInThePast()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            return selectPage.SelectDateInThePast();
+        }
+        public bool CheckEqualNames()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            if (selectPage.GetFirstCity() == selectPage.GetSecondCity())
+                return true;
+            else
+                return false;
+        }
+
+        public bool SelectMorePassengers()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            selectPage.ClickPassengersMenu();
+            for(int i = 0; i< 10; i++)
+            selectPage.SelectPassengerAdult();
+            if (Int32.Parse(selectPage.GetCountAdult()) >= 10)
+                return true;
+            else
+                return false;
+        }
+
+        public bool CheckConvertation(double bestExchangeRate, double worstExchangeRate)
+        {
+    
+            _10UnitTests.ResultPage selectPage = new _10UnitTests.ResultPage(driver);
+            string costBYN = selectPage.GetCostBYN().Trim(new char[] { 'B','Y','N' });
+            selectPage.SelectUSD();
+            string costUSD = selectPage.getCostUSD().Trim(new char[] { 'U', 'S', 'D' });
+            double resultRate = Double.Parse(costBYN) / Double.Parse(costUSD);
+            if (resultRate >= worstExchangeRate && resultRate <= bestExchangeRate)
+                return true;
+            else
+                return false;
+
+        }
+
+        public bool CheckSortByTime()
+        {
+            _10UnitTests.ResultPage selectPage = new _10UnitTests.ResultPage(driver);
+
+            string[] first = selectPage.GetFirstTime().Split(new char[] { ' ' });
+            string[] second = selectPage.GetSecondTime().Split(new char[] { ' ' });
+            string[] third = selectPage.GetSecondTime().Split(new char[] { ' ' });
+            double firstTimeMinutes = Double.Parse(first[0]) * 60 + Double.Parse(first[2]);
+            double secondTimeMinutes = Double.Parse(second[0]) * 60 + Double.Parse(second[2]);
+            double thirdTimeMinutes = Double.Parse(third[0]) * 60 + Double.Parse(third[2]);
+            if (firstTimeMinutes <= secondTimeMinutes && secondTimeMinutes <= thirdTimeMinutes)
+                return true;
+            else
+            return false;
+        }
+        public void SelectSortByTime()
+        {
+            _10UnitTests.ResultPage selectPage = new _10UnitTests.ResultPage(driver);
+            selectPage.StartSort();
+        }
+        public void PlusPassenerBaby()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            selectPage.ClickPassengersMenu();
+            selectPage.SelectPassengerBaby();
+        }
+        public void SelectTicket()
+        {
+            _10UnitTests.ResultPage selectPage = new _10UnitTests.ResultPage(driver);
+            selectPage.ClickTicket();
+        }
+
+        public void InsertNameAdult(string firstName, string lastName)
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            selectPage.InsertNameAdult(firstName, lastName);
+        }
+
+        public void InsertBDAdult(string bDay, string bMonth, string bYear)
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            selectPage.InsertBDAdult(bDay, bMonth, bYear);
+        }
+
+        public void InsertDocAdult(string num, string exDay, string exMonth, string exYear)
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            selectPage.InsertDocAdult(num, exDay, exMonth, exYear);
+        }
+
+        public void InsertNameBaby(string firstName, string lastName)
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            selectPage.InsertNameBaby(firstName, lastName);
+        }
+
+        public void InsertBDBaby(string bDay, string bMonth, string bYear)
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            selectPage.InsertBDBaby(bDay, bMonth, bYear);
+        }
+
+        public void InsertDocBaby(string num, string exDay, string exMonth, string exYear)
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            selectPage.InsertDocBaby(num, exDay, exMonth, exYear);
+       }
+        public bool SubmitTicket()
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            return selectPage.Submit();
+        }
+
+        public void ScrollProfilePAge()
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            selectPage.ScrollPage();
+        }
+        public void InsertInfoIntoProfile(string email, string phone)
+        {
+            _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            selectPage.InsertInfo( email,  phone);
+        }
+        public bool ReturnBAbyAgeError()
+        {
+         _10UnitTests.PassengerProfile selectPage = new _10UnitTests.PassengerProfile(driver);
+            return   selectPage.CheckErrorMessage();
+        }
+        public bool CheckErrorMessage()
+        {
+            _10UnitTests.MainPage selectPage = new _10UnitTests.MainPage(driver);
+            string message;
+            try
+            {
+                message = selectPage.GetErrorMessage();
+            }
+            catch
+            {
+                return true;
+            }
+            if (message.Length == 0)
+                return true;
+            else return false;
+        }
+    }
+}
